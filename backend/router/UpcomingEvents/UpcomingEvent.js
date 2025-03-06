5b28b64ae2346bc62734c0994c770b6fb3717882
syedmohammedzubair1 <syedmohammedzubair913@gmail.com>
2025-03-06 15:29:26 +0530

    Added Upcoming Events Backend

--- backend/router/UpcomingEvents/UpcomingEvent.js
+++ backend/router/UpcomingEvents/UpcomingEvent.js
@@ -13,6 +13,6 @@ router.get("/upcomingEvent", getAllEvents);
 router.get("/upcomingEvent/:id", getEventById);
 router.post("/upcomingEvent", createEvent);
 router.put("/upcomingEvent/:id", updateEvent);
-router.delete("/upcomingEvent:id", deleteEvent);
+router.delete("/upcomingEvent/:id", deleteEvent);
 
 module.exports = router;
