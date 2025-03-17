92d542b195673ff34153df4fcf9c40543597c0c1
janu <janu@gmail.com>
2025-03-17 18:12:26 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import AdminRoutes from "./routes/AdminRoutes";
+
 import CuratorRoutes from "./routes/CuratorRoutes";
+
 import SubscriberRoutes from "./routes/SubscriberRoutes";
+
 import PublicRoutes from "./routes/PublicRoutes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
